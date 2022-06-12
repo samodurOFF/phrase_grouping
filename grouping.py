333f0931195888616e6cef0cf85f47f3b7a612c4
samodurOFF <samodurovdanil@gmail.com>
2022-06-12 22:42:15 +0500

    the filtration function was deleted

--- grouping.py
+++ grouping.py
@@ -172,54 +172,6 @@ def group(df, ignored_list, ignoring_type=None):
     return pd.DataFrame(final_list), pd.DataFrame(ratio_list)
 
 
-def filtration(df):
-    reduced_df = df.loc[:, 'PHRASES':'GROUP']  # сокращение размеров DataFrame
-    groups = list(sorted(set(reduced_df['GROUP'].to_list())))  # список всех групп
-    length = len(groups)  # длина списка с группами, нужна для статус-бара
-    group_phrase_dict = {}  # пустой словаря, где ключи – группы, а значения – список фраз
-    valid_groups = []  # список, где будут сохраняться группы без повторений
-
-    print('Этап III/IV')  # вывести этап работы
-    # цикл для заполнения group_phrase_dir
-    for index, group in enumerate(groups):  # для каждой группы и ее индекса в списке групп
-        print(f'\r{round(index / (length - 1) * 100, 2)}%', end='')  # вывести процент обработанных групп и записать
-        group_phrase_dict[group] = reduced_df.loc[reduced_df['GROUP'] == group]['PHRASES'].to_list()  # в
-        # group_phrase_dir
-
-    print('\nЭтап IV/IV')  # вывести этап работы
-    # цикл, определяющий группы, фразы в которых НЕ содержаться в других группах. Эти группы помещаются в valid_groups
-    index = 0  # начальный индекс
-    while True:  # бесконечный цикл
-        length = len(groups)  # длина списка с группами, нужна для статус-бара
-        print(f'\r{round(index / (length - 1) * 100, 2)}%', end='')  # вывести процент обработанных групп
-        if index == length - 1:  # если индекс последней группы в списке групп то
-            break  # завершить цикл
-
-        group = groups[index]  # основная рабочая группа
-        phrases = group_phrase_dict[group]  # фразы основной рабочей группы
-
-        # цикл для перебора и сравнения фраз последующих групп с фразами основной группы
-        for next_group in groups[index + 1:]:  # для каждой следующей группы
-            phrases_next = group_phrase_dict[next_group]  # определить фразы этой группы
-            intersection = list(set(phrases) & set(phrases_next))  # пересечение списков для основной и текущей группы
-            if intersection == phrases:  # если пересечение фраз равно списку фраз для основной группы, то
-                groups.remove(group)  # удалить из списка групп номер основной группы
-                group = next_group  # и присвоить ему значение текущей группы
-            elif intersection == phrases_next:  # если пересечение фраз равно списку фраз для текущей группы, то
-                groups.remove(next_group)  # удалить из списка групп номер текущей группы
-            else:  # если пересечений нет, то
-                continue  # продолжить цикл с новой текущей группой
-        else:  # по завершению цикла проверить
-            if group == groups[index]:  # если True, то эта группа уникальна и она не была удалена из списка групп
-                valid_groups.append(group)  # добавить номер группы в valid_groups
-                index += 1  # увеличить счетчик цикла и перезапустить цикл с новой основной группой
-            else:  # если эта группа содержалась в другой группе, то она была удалена и под этим индексом будет
-                # уже другая группа, значить индекс увеличивать  е нужно
-                valid_groups.append(group)  # добавить номер группы в valid_groups
-
-    return df[df['GROUP'].isin(valid_groups)]  # вернуть новый DataFrame, содержащий только уникальные группы
-
-
 def save_rest(init_df, final_df, phrases, result_dir, file_dir, N, ignoring):
     phrases_after = sorted(set(final_df['PHRASES'].to_list()))  # все сгруппированные фразы
     diff_list = list(set(phrases) - set(phrases_after))  # фразы, которые не были сгруппированы
@@ -316,9 +268,6 @@ if __name__ == '__main__':
         final_df = unique_search(final_df)
 
         final_df.to_csv(f'{result_dir}/{file_dir}/{file_name}_grouped.csv', sep=';', index=False)  # запись в файл
-
-        # final_df = filtration(final_df)  # вызов функции по удалению групп, содержащихся в других группах
-        # final_df.to_csv(f'{result_dir}/{file_dir}/{file_dir}_filtered.csv', sep=';', index=False)  # запись в файл
 
         # if input('Провести группировку по URL (y/n)?\nОтвет: ') == 'y':
         #     N2 = int(input('Укажите количество пересечений N: '))  # количество пересечений
